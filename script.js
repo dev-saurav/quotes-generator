b44ea42e9e1645c63f40925bfd17420fafb74315
webDevSaurav <webdevelopersaurav@gmail.com>
2020-07-25 17:30:34 +0530

    added the api and api calling function with CORS compatibility

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello")
+//get quotes from the api
+async function getQuote() {
+    const proxy = "https://desolate-bastion-82740.herokuapp.com/"
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+    try {
+        const response = await fetch(proxy + apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // if we get a error then call for a new quote
+        getQuote()
+        console.log("oops!", error);
+    }
+}
+
+
+//on load
+getQuote();
